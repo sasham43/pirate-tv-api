867fc5544cb42a9c87e88d802d62e569787ea63b
sasham43 <sashasemail@gmail.com>
2020-01-16 19:47:36 -0500

    initial

---
+++ src/server.js
@@ -0,0 +1,37 @@
+// require("dotenv").config();
+var express = require('express');
+var bodyParser = require('body-parser');
+// var config = require('./config').get();
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: '50mb'
+}));
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+var listenPort = process.env.PORT || 3005;
+
+// app.use('/videos', videos);
+// app.use('/assistant', assistant);
+// app.use('/', root);
+
+app.use('/', function(req, res, next){
+    res.send('10-4 good buddy')
+})
+
+app.use(function(err, req, res, next){
+  console.log('error:', err);
+  res.status(err.statusCode || 500).json(err);
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/root/public/index.html');
+});
+
+app.listen(listenPort, function(){
+  console.log('server listening on port', listenPort + '...');
+});
